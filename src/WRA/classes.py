71bd427d07ef64281c8dddc7ea60ff8f2b175658
Viktor-Johnsen <viktor.gjohnsen@gmail.com>
2025-04-27 11:51:36 +0200

    Moved calculate_wind_at_height() from InterpolatedSite to WindCalculations, made a function for loading multiple .nc-files from a folder path, and finally made a function to compare AEPs for different years at a specific location

--- src/WRA/classes.py
+++ src/WRA/classes.py
@@ -7,16 +7,22 @@ import matplotlib.ticker as mtick
 from windrose import WindroseAxes
 from scipy.integrate import quad  # for AEP
 
-
-def pdf_weib(k, A, u):
-    return (k / A) * (u / A)**(k - 1) * np.exp(- (u / A)**k)
+# import functions -> wrong because of:
+# "sys.path.insert(0, __file__.replace('main.py', '../src'))" in main.py...
+from WRA import functions
 
 
 class WindCalculation():
-    def __init__(self, v_x, v_y, heights):
+    def __init__(self, v_x, v_y, height_point, ref_heights):
+        if not (ref_heights[0] <= height_point <= ref_heights[1]):
+            raise ValueError(
+                f"Height '{height_point} m' is not between the two "
+                + f"reference heights: ({ref_heights} m). Try another value.")
+        ### Noget med en exception error hvis height_point ikke er inden for ref_heights. Måske ikke her, men et andet sted? -- Synes det passer godt ind her. Har sat en ind nedenunder.
         self.v_x = v_x
         self.v_y = v_y
-        self.h = heights
+        self.h_point = height_point
+        self.h_ref = ref_heights
 
     def get_velocity_site(self):
         v = np.sqrt(self.v_x**2 + self.v_y**2)
@@ -34,29 +40,37 @@ class WindCalculation():
         return theta_deg
 
     def get_alpha(self):
-        z, z_r = self.h
+        z, z_r = self.h_ref
         u_z, u_z_ref = self.get_velocity_site()
         alpha = np.log(u_z/u_z_ref) / np.log(z/z_r)
         return alpha
 
+    def calculate_wind_at_height(self):
+        alpha = self.get_alpha()
+        u_ref = self.get_velocity_site()[0]
+        u_z = u_ref * (self.h_point/self.h_ref[0])**alpha
+        return u_z
+
 
 class DataSite(WindCalculation):
-    def __init__(self, dataset, latitude, longitude, heights=[10, 100], name=None):
+    def __init__(self, dataset, latitude, longitude, height_point=10,
+                 ref_heights=[10, 100], name=None):
         self.data = dataset
         self.lat = latitude
         self.lon = longitude
-        self.h = heights
+        self.h_point = height_point
+        self.h_ref = ref_heights
         self.name = name
         self.data_length = self.data.sizes['valid_time']
 
         self.v_x, self.v_y = self.load_components()
-        super().__init__(self.v_x, self.v_y, self.h)
+        super().__init__(self.v_x, self.v_y, self.h_point, self.h_ref)
 
     def load_components(self):
         v_x = np.empty((2, self.data_length))
         v_y = np.empty((2, self.data_length))
 
-        for i, h in enumerate(self.h):  # idx 1 is reference point
+        for i, h in enumerate(self.h_ref):  # idx 1 is reference point
 
             v_x[i] = self.data[f'u{h}'].sel(latitude=self.lat,
                                             longitude=self.lon).values
@@ -67,12 +81,8 @@ class DataSite(WindCalculation):
 
 
 class InterpolatedSite(WindCalculation):
-    def __init__(self, dataset, latitude_point, longitude_point, height_point, ref_heights=[10, 100], name=None):
-        if not (ref_heights[0] <= height_point <= ref_heights[1]):
-            raise ValueError(
-                f"Height '{height_point} m' is not between the two "
-                + f"reference heights: ({ref_heights} m). Try another value.")
-        ### Noget med en exception error hvis height_point ikke er inden for ref_heights. Måske ikke her, men et andet sted? -- Synes det passer godt ind her. Har sat en ind nedenunder.
+    def __init__(self, dataset, latitude_point, longitude_point,
+                 height_point, ref_heights=[10, 100], name=None):
         self.data = dataset
         self.lat_point = latitude_point
         self.lon_point = longitude_point
@@ -89,7 +99,8 @@ class InterpolatedSite(WindCalculation):
         self.v_x_point = np.vstack([interp_dict[f'u{h}'] for h in self.h_ref])
         self.v_y_point = np.vstack([interp_dict[f'v{h}'] for h in self.h_ref])
 
-        super().__init__(self.v_x_point, self.v_y_point, self.h_ref)
+        super().__init__(self.v_x_point, self.v_y_point,
+                         self.h_point, self.h_ref)
         # Initializing the parent class with the desired wind speed components
         # from the point as well as the refence heights for alpha calculation
         self.angle_point_rad, self.angle_point_deg = self.interpolate_angle()
@@ -127,11 +138,6 @@ class InterpolatedSite(WindCalculation):
 
         return interp_dict
 
-    def calculate_wind_at_height(self):
-        alpha = self.get_alpha()
-        u_ref = self.get_velocity_site()[0]
-        u_z = u_ref * (self.h_point/self.h_ref[0])**alpha
-        return u_z
     
     def interpolate_angle(self):
         thetas_h_ref = np.unwrap(self.get_angle_rad(), axis=0)
@@ -166,7 +172,7 @@ class InterpolatedSite(WindCalculation):
         k = params[0]
 
         u = np.linspace(0, wind_speeds.max()+5, 100)  ### Måske vi bare skal sætte den her til (0, 25) det er rimelig typisk -- Det kan vi godt gøre for min skyld
-        pdf = pdf_weib(k, A, u)
+        pdf = functions.pdf_weib(k, A, u)
         if show_plot:
             plt.figure(figsize=(8, 5))
             plt.hist(wind_speeds, bins=30, density=True, alpha=0.6,
@@ -227,13 +233,13 @@ class InterpolatedSite(WindCalculation):
  
         # Combining power curve and weibull functions to a single integrand
         def integrand(u):
-            return p_u(u) * pdf_weib(k, A, u)
+            return p_u(u) * functions.pdf_weib(k, A, u)
 
         hourly_avg, _ = quad(integrand, u_in, u_out,
                              limit=100, epsabs=5e-06, epsrel=5e-06)
         # "_" is simply the estimated absolute integration error
 
-        AEP = eta * 8760 * hourly_avg
+        AEP = eta * 8760 * hourly_avg / 10**3  # MWh
 
         p_rated = max(power_curve['Power [kW]'])
         CF = hourly_avg / p_rated
@@ -251,3 +257,64 @@ class InterpolatedSite(WindCalculation):
             plt.show()
 
         return AEP, CF
+
+    def compare_AEPs(self, power_curve, show_comparison=True):
+        p_rated = max(power_curve['Power [kW]'])
+        years_col = pd.to_datetime(self.data.valid_time.values).year
+        years = np.unique(years_col)
+        AEPs = {}
+        CFs = {}
+        for year in years:
+            AEPs[f"{year}"], CFs[f"{year}"] = self.get_AEP(power_curve,
+                                                           year=year)
+            
+        if show_comparison:
+            # For plot design:
+            linestyles = ['dotted', 'dashdot', 'dashed']
+            metric_labels = ['min', 'mean', 'max']
+
+            fig, ax = plt.subplots(figsize=(8, 5))
+            bars = ax.bar(x=years, height=list(AEPs.values()),
+                          color=plt.cm.tab10.colors[1])
+            ax.set_ylabel("AEP [MWh]", fontsize=13)
+            # Set alpha for every other bar to easily distinguish between years
+            for i, bar in enumerate(bars):
+                if i % 2 == 0:
+                    bar.set_alpha(0.75)
+                else:
+                    bar.set_alpha(1)
+            # To see every year on the x-axis:
+            ax.set_xticks(np.arange(years[0], years[-1]+1))
+            ax.set_xticklabels(years, rotation=45, fontsize=13)
+            # To remove the frame and ticks::
+            ax.spines[['top', 'bottom', 'left', 'right']].set_visible(False)
+            ax.tick_params(bottom=False, left=False)
+            # To only see the gridlines on the y-axis:
+            ax.yaxis.grid(True, color="#EEEEEE")
+            ax.xaxis.grid(False)
+            # To plot the min, mean, and max AEP:
+            for j, metric in enumerate([min(list(AEPs.values())), np.mean(list(AEPs.values())), max(list(AEPs.values()))]):
+                ax.hlines(metric, years[0]-0.5, years[-1]+0.5,
+                          color='k', alpha=0.85,
+                          linestyles=linestyles[j],
+                          label=metric_labels[j])
+                # To annotate the values of the metrics:
+                ax.text(years[-1]+1.2, metric,
+                        f'{metric:.0f} MWh',  # format the number (1 decimal)
+                        va='center',
+                        ha='left',
+                        fontsize=12,
+                        color='k',
+                        alpha=0.85)
+                
+            plt.subplots_adjust(right=1.2)
+            ax.set_title(f"{p_rated/10**3:.0f} MW reference turbine"
+                         + f" (Latitude: {self.lat_point},"
+                         + f" longitude: {self.lon_point},"
+                         + f" and height: {self.h_point} m)",
+                         pad=0)
+            ax.legend(loc='best', fontsize=12,
+                      bbox_to_anchor=(0.97, 0.75))
+            plt.show()
+
+        return AEPs, CFs
